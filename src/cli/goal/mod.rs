67551c052e82700b9a653be52532340510f7e468
Perry <jungledesh@gmail.com>
2026-05-13 16:13:49 -0700

    Ceiling fix

--- src/cli/goal/mod.rs
+++
@@ -1,9 +0,0 @@
-mod feasibility;
-mod inference;
-mod prompt;
-pub mod types;
-
-pub use feasibility::check_feasibility;
-pub use inference::infer_objective;
-pub use prompt::prompt_goal;
-pub use types::{FeasibilityResult, Goal};
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,7 +1,6 @@
 //! CLI: parse commands, print results.
 
 mod diagnose;
-pub mod goal;
 
 use clap::{CommandFactory, Parser, Subcommand};
 use std::time::Duration;
